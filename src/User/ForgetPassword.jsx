4c1008377cc4121a0d8fe058ff9a9b81d914f61a
Malay Srivastava <57169971+malaysrivastava@users.noreply.github.com>
2020-09-30 16:17:29 +0530

    Forgot-done

--- src/User/ForgetPassword.jsx
+++ src/User/ForgetPassword.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap'
+import Regauth from '../Assests/Authentication Illustration.svg';
+
 
 const ForgetPassword = ({history}) => {
   const [formData, setFormData] = useState({
@@ -45,16 +47,19 @@ const ForgetPassword = ({history}) => {
   return (
     <div >
       <ToastContainer />
-      <div className="conatiner m-1 centered p-2">
-      <div className="row m-auto text-center">
-      <div className="col-sm-12">
-      <h1 className='text-2xl m-3 text-center font-extrabold'> Forget Password </h1>
+      <div className="container m-1 centered p-2">
+      <div className="row m-auto ">
+      <div className="col-sm-5 p-3">
+      <img src={Regauth} style={{width:'100%'}}/>
+     </div>
+     <div className="col-sm-4 offset-sm-3 pt-5">
+     <h1 className='text-2xl m-3 text-center font-extrabold'> Forget Password </h1>
       <form className="form" onSubmit={handleSubmit}>
 
       <div className="form-group">
         <input className="form-control" type="email" placeholder="Enter email" onChange={handleChange('email')} value={email} />
         </div>
-      <Button variant="danger" type="submit" style={{width:'100%'}}> Submit </Button>
+      <Button variant="success" type="submit" style={{width:'100%'}}> Submit </Button>
       </form>
       </div>
       </div>
